51ca97b33f051d4c8bfe4881e822ffd61641a348
Zireael07 <zi@mail.com>
2017-04-20 15:08:53 +0200

    car slows down when not pressing gas, add reverse gear

--- game/car/vehicle.gd
+++ game/car/vehicle.gd
@@ -10,8 +10,12 @@ var steer_target = 0
 
 export var engine_force = 40
 
+#speed
+var speed
 
 func _fixed_process(delta):
+	speed = get_linear_velocity().length();
+	
 	if (Input.is_action_pressed("ui_left")):
 		steer_target = -STEER_LIMIT
 	elif (Input.is_action_pressed("ui_right")):
@@ -19,13 +23,24 @@ func _fixed_process(delta):
 	else:
 		steer_target = 0
 	
+	#gas
 	if (Input.is_action_pressed("ui_up")):
 		set_engine_force(engine_force)
 	else:
-		set_engine_force(0)
+		if (speed > 3):
+			set_engine_force(-engine_force/4)
+		else:
+			set_engine_force(0)
 	
+	#brake/reverse
 	if (Input.is_action_pressed("ui_down")):
-		set_brake(1)
+		if (speed > 5):
+			#slows down 1 unit per tick
+			set_brake(1)
+		else:
+			#reverse
+			set_brake(0.0)
+			set_engine_force(-engine_force)
 	else:
 		set_brake(0.0)
 	
