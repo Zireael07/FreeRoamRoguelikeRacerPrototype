617cecfe9929cdf91a63b5f2b66ac3ab9812cda7
Zireael07 <zi@mail.com>
2017-05-30 13:58:08 +0200

    make a basic main menu

---
+++ game/hud/new_game_button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+func _on_new_game_pressed():
+	#print("Pressed button")
+	get_tree().change_scene("res://scenes/Main.tscn")
+	
+	pass
