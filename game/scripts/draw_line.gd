3ddeb51e54213943b1a83d293d919b9dd38d648c
Zireael07 <zi@mail.com>
2017-10-09 14:15:50 +0200

    updates to draw line script

--- game/scripts/draw_line.gd
+++ game/scripts/draw_line.gd
@@ -4,17 +4,35 @@ extends GeometryInstance
 # class member variables go here, for example:
 var m = FixedMaterial.new()
 var points = Vector3Array()
+export(Color) var color = Color(1,1,1)
+
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	set_material()
 	
+
+func set_material():
 	m.set_line_width(3)
 	m.set_point_size(3)
 	m.set_fixed_flag(FixedMaterial.FLAG_USE_POINT_SIZE, true)
 	m.set_flag(Material.FLAG_UNSHADED, true)
 	
+	m.set_parameter(FixedMaterial.PARAM_DIFFUSE, color)
+	
+	#m.set_fixed_flag(FixedMaterial.FLAG_USE_COLOR_ARRAY, true)
+	
 	set_material_override(m)
+
+func set_material_color(color):	
+	m.set_line_width(3)
+	m.set_point_size(3)
+	m.set_fixed_flag(FixedMaterial.FLAG_USE_POINT_SIZE, true)
+	m.set_flag(Material.FLAG_UNSHADED, true)
+	
+	m.set_parameter(FixedMaterial.PARAM_DIFFUSE, color)
+	
+	set_material_override(m)
+
 
 
 func draw_line(points):
@@ -25,3 +43,18 @@ func draw_line(points):
 	end()
 	
 	pass
+
+func draw_line_color(points, size, color):
+	set_material_color(color)	
+	
+	clear()
+	
+	if (size != null):
+		##set width
+		m.set_line_width(size)
+		m.set_point_size(size)
+	
+	begin(Mesh.PRIMITIVE_LINE_STRIP, null)
+	for i in points:
+		add_vertex(i)
+	end()	
