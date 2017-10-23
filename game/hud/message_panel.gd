b0cf42379ee53435163641d83f47be532a4e6565
Zireael07 <zi@mail.com>
2017-10-23 19:45:08 +0200

    message panel now has an optional OK button

--- game/hud/message_panel.gd
+++ game/hud/message_panel.gd
@@ -3,11 +3,13 @@ extends Control
 # class member variables go here, for example:
 var label
 var button
+var ok_button
 
 func _ready():
 	
 	label = get_node("Label")
 	button = get_node("Button")
+	ok_button = get_node("OK_button")
 	
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -21,4 +23,17 @@ func _on_Button_pressed():
 	hide()
 	# get_parent().remove_child(self)
 	
+	pass # replace with function body
+
+
+func enable_ok(val):
+	if val:
+		ok_button.show()
+		ok_button.set_disabled(false)
+	else:
+		ok_button.hide()
+		ok_button.set_disabled(true)
+
+func _on_OK_button_pressed():
+	hide()
 	pass # replace with function body
