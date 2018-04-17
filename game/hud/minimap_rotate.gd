e5986052e05b1a49b4ba5cb9404d41ff3b131ad1
Zireael07 <zi@mail.com>
2018-04-17 11:33:37 +0200

    fix: map was rotating the wrong direction

--- game/hud/minimap_rotate.gd
+++ game/hud/minimap_rotate.gd
@@ -4,10 +4,21 @@ extends Node2D
 
 func _ready():
 	#set_process_input(true)
-	set_physics_process(true)
+	#set_physics_process(true)
+	set_process(true)
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
+
+func _process(delta):
+	var player_rot = get_parent().get_parent().get_parent().get_rotation()
+	var map_rot = player_rot.y
+	
+	#this resolves the gimbal lock issues
+	if (player_rot.x < -deg2rad(150) or player_rot.x > deg2rad(150)) and (player_rot.z < - deg2rad(150) or player_rot.z > deg2rad(150)):
+		map_rot = deg2rad(180)+player_rot.y #1.02
+	
+	set_rotation(map_rot)
 
 func _physics_process(delta):
 	var player_rot = get_parent().get_parent().get_parent().get_rotation()
