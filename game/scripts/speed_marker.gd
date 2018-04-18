b9a78c9fdc4026877cc3d0134eb84bbe3b4bfd1a
Zireael07 <zi@mail.com>
2018-04-18 11:29:11 +0200

    new: speed trap

---
+++ game/scripts/speed_marker.gd
@@ -0,0 +1,68 @@
+extends Spatial
+
+# class member variables go here, for example:
+var player
+var player_script
+var time
+var count
+#start/finish
+#var finish = false
+#var start
+
+#export var target = Vector3()
+
+func _ready():
+	player_script = load("res://car/vehicle_player.gd")
+	count = false
+
+	#set_process(true)
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _on_Area_body_enter( body ):
+	if body is VehicleBody:
+		if body is player_script:
+			print("Area entered by the player - speed")
+			player = body
+			
+			var speed = player.get_linear_velocity().length()
+			var speed_kph = round(speed*3.6)
+
+			var msg = body.get_node("Messages")
+			#msg.set_initial(false)
+			msg.set_text("TEST SPEED! " + "\n" + "Speed at marker is " + str(speed_kph))
+			msg.get_node("OK_button").connect("pressed", self, "_on_ok_click")
+			msg.enable_ok(true)
+			msg.show()
+		#else:
+		#	print("Area entered by a car " + body.get_parent().get_name())
+	#else:
+	#	print("Area entered by something else")
+
+
+func _on_ok_click():
+	#count = true
+	#time = 0.0
+	#spawn_finish(self)
+	print("Clicked ok!")
+	var msg = player.get_node("Messages")
+	msg.hide()
+
+
+func _process(delta):
+	if count:
+		time += delta
+		#print("Timer is " + str(time))
+		player.get_node("root").get_node("Label timer").show()
+		player.get_node("root").update_timer(str(time))
+	#else:
+	#	print("Count is off")
+
+func _on_Area_body_exit( body ):
+	if body is VehicleBody:
+		if body is player_script:
+			print("Area exited by the player")
+			player = body
+			#var msg = body.get_node("Messages")
+			#msg.hide()
