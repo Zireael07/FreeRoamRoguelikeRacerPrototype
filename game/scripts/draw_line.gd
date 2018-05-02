3d758147c0f0ef4ed9236d420fb3803196554bc0
Zireael07 <zi@mail.com>
2018-05-02 17:22:27 +0200

    new: function to debug draw arc in 3d

--- game/scripts/draw_line.gd
+++ game/scripts/draw_line.gd
@@ -67,3 +67,42 @@ func draw_line_color(points, size, color_par):
 		#set_color(color)
 		add_vertex(i)
 	end()	
+
+func get_circle_arc_poly(center, radius, angle_from, angle_to):
+	# from 3d to 2d
+	center = Vector2(center.x, center.z)
+	# usual arc stuff
+	var nb_points = 32
+	var points_arc = PoolVector2Array()
+	points_arc.push_back(center)
+	var colors = PoolColorArray([color])
+
+	for i in range(nb_points+1):
+		var angle_point = angle_from + i*(angle_to-angle_from)/nb_points
+		points_arc.push_back(center + Vector2( cos( deg2rad(angle_point) ), sin( deg2rad(angle_point) ) ) * radius)
+	
+	# to 3D
+	var points_arc_3d = PoolVector3Array()
+	
+	for p in points_arc:
+		points_arc_3d.push_back(Vector3(p.x, 1, p.y))
+	
+	# end once again
+	points_arc_3d.push_back(Vector3(center.x, 1, center.y))
+	
+	return points_arc_3d
+	
+func draw_arc_poly(center, rot, angle, color_par):
+	var points = []
+	clear()
+	
+	color = color_par
+	set_material()
+	
+	points = get_circle_arc_poly(center, 2, rot, rot+angle)
+	
+	#begin(Mesh.PRIMITIVE_TRIANGLES,null)
+	begin(Mesh.PRIMITIVE_LINE_STRIP, null)
+	for i in points:
+		add_vertex(i)
+	end()
