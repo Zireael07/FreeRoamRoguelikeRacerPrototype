d62974a8c17c32941499b225bf2aba78130a6309
Zireael07 <zi@mail.com>
2018-05-25 17:23:32 +0200

    fix: reduce number of points in AI path for a speed boost on straights

--- game/car/AI_pathing.gd
+++ game/car/AI_pathing.gd
@@ -28,12 +28,12 @@ func find_path():
 		#print("We have a navigation node")
 		
 		# enable the navmesh we're interested in
-		if (left):
-			get_tree().call_group("right_lane", "set_enabled", false)
-			get_tree().call_group("left_lane", "set_enabled", true)
-		else:
-			get_tree().call_group("left_lane", "set_enabled", false)
-			get_tree().call_group("right_lane", "set_enabled", true)
+#		if (left):
+#			get_tree().call_group("right_lane", "set_enabled", false)
+#			get_tree().call_group("left_lane", "set_enabled", true)
+#		else:
+#			get_tree().call_group("left_lane", "set_enabled", false)
+#			get_tree().call_group("right_lane", "set_enabled", true)
 		
 		# get the points on navmesh relative to ourselves and target
 		var pos = get_translation()
@@ -46,13 +46,48 @@ func find_path():
 		
 		
 		if (path.size() > 0):
+			print(get_name() + " has a path: " + str(path.size()))
 			#print(get_name() + " has a path " + String(path))
-			print(get_name() + " AI has a path")				
+			#print(get_name() + " AI has a path")				
 			
-			return path
+			# reduce number of points
+			var new_path = reduce_path(path)
+			return new_path
+			
+			#return path
 		else:
 			#print("No path")
 			return null
+
+func reduce_path(path):
+	var new_path = Array(path).duplicate() # can't iterate and remove
+			
+	var to_remove = []
+	# size()-1 is normal, deduce 2 so that i-2 works:
+	for i in path.size()-3:
+		# B-A = A to B
+		var vec1 = path[i+1]-path[i]
+		var vec2 = path[i+2]-path[i]
+		var angle = vec2.angle_to(vec1) #radians
+		#print("Angle diff " + str(rad2deg(angle)) + " for i: " + str(i))
+		
+		# if angle is the same, remove middle point
+		if rad2deg(angle) < 0.01:
+			#print("Removing point at: " + str(i+1) + " because angle is " + str(rad2deg(angle)))
+			
+			to_remove.append(path[i+1])
+			# as we remove, the indices change
+			#new_path.remove(i+1)
+	
+	# remove specified
+	for p in to_remove:
+		new_path.remove(new_path.find(p))
+	
+	
+	#print("New path" + str(new_path))
+	print("New path: " + str(new_path.size()))
+		
+	return new_path
 			
 func debug_cube(loc):
 	var mesh = CubeMesh.new()
