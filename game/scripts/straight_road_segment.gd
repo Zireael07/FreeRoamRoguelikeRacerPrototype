201dc1a8a3c7bf054774bad1d70264eb5e1262a8
Zireael07 <zi@mail.com>
2019-06-25 20:18:27 +0200

    fix: factor out making road to a function

--- game/scripts/straight_road_segment.gd
+++ game/scripts/straight_road_segment.gd
@@ -78,154 +78,165 @@ func _ready():
 	
 	#print("Positions: " + str(positions.size()))
 	
+	
+	#overdraw fix
+	if (get_parent().get_name().find("Spatial") != -1):
+		makeRoad()
+		
+	#place props
+	get_node("Spatial").place_props(trees, bamboo, sectionlength*length)
+
+func makeRoad():
 	var quads = []
 	#var support_quads = []
 	var rail_quads = []
 	
-	#overdraw fix
-	if (get_parent().get_name().find("Spatial") != -1):
-		# find out slope
-		var slope_diff = road_slope/length
-		#print("Slope diff" + str(slope_diff))
+	# find out slope
+	var slope_diff = road_slope/length
+	#print("Slope diff" + str(slope_diff))
 		
-		for index in range(length):
-			#print("Index " + str(index))
-			#clear the array
-			temp_positions.resize(0)
-			support_positions.resize(0)
-			rail_positions_left.resize(0)
-			rail_positions_right.resize(0)
-			
-			
-			var start = Vector3(0,roadheight+(index*slope_diff),index*sectionlength)
-			initSection(start, slope_diff)
-	
-			if slope_diff > 0:
-				# one time for the whole road (!)
-				makeSupport(start, slope_diff)
-				#support_quads.append(getQuadsSimple(support_positions))
-			
-	
-			#mesh
-			#var num = temp_positions.size()
-			#for index in range(num):
-				#print("Index from temp_positions " + str(index))
-				##draw_debug_point(positions[index], color)
-			#only make the mesh in game (meshing in editor is hilariously slow, up to 900 ms)
-			#if not Engine.is_editor_hint() or Engine.is_editor_hint():
-				#meshCreate(temp_positions, material)
-			
-			quads.append(getQuads(temp_positions)[0])
-			quads.append(getQuads(temp_positions)[1])
-			
-			positions.push_back(temp_positions[1])
-			positions.push_back(temp_positions[2])
-			left_positions.push_back(temp_positions[0])
-			left_positions.push_back(temp_positions[3])
-			right_positions.push_back(temp_positions[4])
-			right_positions.push_back(temp_positions[5])
-			
-			if sidewalks:
-				#print("We have sidewalks or guardrails, need more positions")
-				points_inner_side.push_back(temp_positions[6])
-				points_inner_side.push_back(temp_positions[7])
-				points_outer_side.push_back(temp_positions[8])
-				points_outer_side.push_back(temp_positions[9])
-				
-			
-			if guardrails and not sidewalks:
-				points_inner_rail.push_back(temp_positions[6])
-				points_inner_rail.push_back(temp_positions[7])
-				points_outer_rail.push_back(temp_positions[8])
-				points_outer_rail.push_back(temp_positions[9])
-			
-				# guardrail quads
-				rail_positions_left.push_back(temp_positions[6]) #0
-				rail_positions_left.push_back(temp_positions[7]) #1
-				rail_positions_left.push_back(temp_positions[3]) #2
-				rail_positions_left.push_back(temp_positions[0]) #3
-				
-				rail_quads.append(getQuadsSimple(rail_positions_left))
-			
-				rail_positions_right.push_back(temp_positions[8]) #0
-				rail_positions_right.push_back(temp_positions[9]) #1
-				rail_positions_right.push_back(temp_positions[5]) #2
-				rail_positions_right.push_back(temp_positions[4]) #3
-				
-				rail_quads.append(getQuadsSimple(rail_positions_right))
-				
-			# navmesh margin
-			#left_nav_positions.push_back(temp_positions[6])
-			#left_nav_positions.push_back(temp_positions[7])
-			#right_nav_positions.push_back(temp_positions[8])
-			#right_nav_positions.push_back(temp_positions[9])
-			
-			
+	for index in range(length):
+		#print("Index " + str(index))
+		#clear the array
+		temp_positions.resize(0)
+		support_positions.resize(0)
+		rail_positions_left.resize(0)
+		rail_positions_right.resize(0)
+		
+		
+		var start = Vector3(0,roadheight+(index*slope_diff),index*sectionlength)
+		initSection(start, slope_diff)
+
+		if slope_diff > 0:
+			# one time for the whole road (!)
+			makeSupport(start, slope_diff)
+			#support_quads.append(getQuadsSimple(support_positions))
+		
+
+		#mesh
+		#var num = temp_positions.size()
+		#for index in range(num):
+			#print("Index from temp_positions " + str(index))
+			##draw_debug_point(positions[index], color)
+		#only make the mesh in game (meshing in editor is hilariously slow, up to 900 ms)
+		#if not Engine.is_editor_hint() or Engine.is_editor_hint():
+			#meshCreate(temp_positions, material)
+		
+		quads.append(getQuads(temp_positions)[0])
+		quads.append(getQuads(temp_positions)[1])
+
+		positions.push_back(temp_positions[1])
+		positions.push_back(temp_positions[2])
+		left_positions.push_back(temp_positions[0])
+		left_positions.push_back(temp_positions[3])
+		right_positions.push_back(temp_positions[4])
+		right_positions.push_back(temp_positions[5])
+		
+		if sidewalks:
+			#print("We have sidewalks or guardrails, need more positions")
+			points_inner_side.push_back(temp_positions[6])
+			points_inner_side.push_back(temp_positions[7])
+			points_outer_side.push_back(temp_positions[8])
+			points_outer_side.push_back(temp_positions[9])
 			
 		
-		if Engine.is_editor_hint() or not Engine.is_editor_hint():
-			#setupNavi(self)
-			optimizedmeshCreate(quads, material)
+		if guardrails and not sidewalks:
+			points_inner_rail.push_back(temp_positions[6])
+			points_inner_rail.push_back(temp_positions[7])
+			points_outer_rail.push_back(temp_positions[8])
+			points_outer_rail.push_back(temp_positions[9])
+		
+			# guardrail quads
+			rail_positions_left.push_back(temp_positions[6]) #0
+			rail_positions_left.push_back(temp_positions[7]) #1
+			rail_positions_left.push_back(temp_positions[3]) #2
+			rail_positions_left.push_back(temp_positions[0]) #3
 			
-			if support_positions.size() > 0:
-				#optimizedmeshCreate(support_quads, building_tex1)
-				var array = support_positions
-				var surface = SurfaceTool.new()
-				surface.begin(Mesh.PRIMITIVE_TRIANGLES)
-
-				#Create a node building that will hold the mesh
-				var node = MeshInstance.new()
-				node.set_name("support")
-				add_child(node)
-
-				# cement material is one sided for now
-				addQuad(array[0], array[1], array[2], array[3], cement_tex, surface, false)
-				addQuad(array[3], array[2], array[1], array[0], cement_tex, surface, false)
-				
-				# other side
-				addQuad(array[4], array[5], array[6], array[7], cement_tex, surface, false)
-				addQuad(array[7], array[6], array[5], array[4], cement_tex, surface, false)
-				
-
-				#Set the created mesh to the node
-				node.set_mesh(surface.commit())
+			rail_quads.append(getQuadsSimple(rail_positions_left))
+		
+			rail_positions_right.push_back(temp_positions[8]) #0
+			rail_positions_right.push_back(temp_positions[9]) #1
+			rail_positions_right.push_back(temp_positions[5]) #2
+			rail_positions_right.push_back(temp_positions[4]) #3
 			
-			if rail_quads.size() > 0:
-				var surface = SurfaceTool.new()
-				surface.begin(Mesh.PRIMITIVE_TRIANGLES)
-
-				#Create a node building that will hold the mesh
-				var node = MeshInstance.new()
-				node.set_name("guardrail")
-				add_child(node)
+			rail_quads.append(getQuadsSimple(rail_positions_right))
 			
-				var uvs = [Vector2(0,0), Vector2(0,1), Vector2(1,1), Vector2(1,0)]
-			
-				for qu in rail_quads:
-					addQuadCustUV(qu[0], qu[1], qu[2], qu[3], uvs[1], uvs[2], uvs[3], uvs[0], railing_tex, surface) #, qu[4])
-					addQuadCustUV(qu[3], qu[2], qu[1], qu[0], uvs[0], uvs[3], uvs[2], uvs[1], railing_tex, surface) #, qu[4])
-					# 3 2 1 0
+		# navmesh margin
+		#left_nav_positions.push_back(temp_positions[6])
+		#left_nav_positions.push_back(temp_positions[7])
+		#right_nav_positions.push_back(temp_positions[8])
+		#right_nav_positions.push_back(temp_positions[9])
+		
+		
+		
 	
-				surface.generate_normals()
-	
-				surface.index()
-				
-				#Set the created mesh to the node
-				node.set_mesh(surface.commit())
-				
-				#Turn off shadows
-				node.set_cast_shadows_setting(0)
-				
-				# yay GD 3
-				#node.create_convex_collision()
-				node.create_trimesh_collision()
+	if Engine.is_editor_hint() or not Engine.is_editor_hint():
+		#setupNavi(self)
+
+		# road cap		
+		if length-int(length) > 0:
+			roadCap((length-int(length)), cap_quads, slope_diff)
+		
+		optimizedmeshCreate(quads, cap_quads, material)
+		
+		# bonus stuffs
+		if support_positions.size() > 0:
+			#optimizedmeshCreate(support_quads, building_tex1)
+			var array = support_positions
+			var surface = SurfaceTool.new()
+			surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+
+			#Create a node that will hold the mesh
+			var node = MeshInstance.new()
+			node.set_name("support")
+			add_child(node)
+
+			# cement material is one sided for now
+			addQuad(array[0], array[1], array[2], array[3], cement_tex, surface, false)
+			addQuad(array[3], array[2], array[1], array[0], cement_tex, surface, false)
 			
-		if not Engine.is_editor_hint():
-			# disable the emissiveness
-			reset_lite()
-				
-	#set the end
-	relative_end = Vector3(0,0, sectionlength*length)
+			# other side
+			addQuad(array[4], array[5], array[6], array[7], cement_tex, surface, false)
+			addQuad(array[7], array[6], array[5], array[4], cement_tex, surface, false)
+			
+
+			#Set the created mesh to the node
+			node.set_mesh(surface.commit())
+		
+		if rail_quads.size() > 0:
+			var surface = SurfaceTool.new()
+			surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+
+			#Create a node building that will hold the mesh
+			var node = MeshInstance.new()
+			node.set_name("guardrail")
+			add_child(node)
+		
+			var uvs = [Vector2(0,0), Vector2(0,1), Vector2(1,1), Vector2(1,0)]
+		
+			for qu in rail_quads:
+				addQuadCustUV(qu[0], qu[1], qu[2], qu[3], uvs[1], uvs[2], uvs[3], uvs[0], railing_tex, surface) #, qu[4])
+				addQuadCustUV(qu[3], qu[2], qu[1], qu[0], uvs[0], uvs[3], uvs[2], uvs[1], railing_tex, surface) #, qu[4])
+				# 3 2 1 0
+
+			surface.generate_normals()
+
+			surface.index()
+			
+			#Set the created mesh to the node
+			node.set_mesh(surface.commit())
+			
+			#Turn off shadows
+			node.set_cast_shadows_setting(0)
+			
+			# yay GD 3
+			#node.create_convex_collision()
+			node.create_trimesh_collision()
+		
+	if not Engine.is_editor_hint():
+		# disable the emissiveness
+		reset_lite()
+			
 	
 	#debug midpoint
 	if positions.size() > 0:
@@ -242,9 +253,6 @@ func _ready():
 		
 		start_ref = positions[0]+start_vector
 		end_ref = positions[positions.size()-1]+end_vector
-	
-	#place props
-	get_node("Spatial").place_props(trees, bamboo, sectionlength*length)
 	
 	#in editor, we draw simple immediate mode lines instead
 	if Engine.is_editor_hint():
@@ -270,8 +278,6 @@ func _ready():
 	# kill debug draw in game
 	else:
 		draw.queue_free()
-	
-	#pass
 
 func initSection(start, slope):
 	var start_height = start.y
