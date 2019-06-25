166eca01c919001e0c0411fe58554d2613b951c5
Zireael07 <zi@mail.com>
2019-06-25 20:11:16 +0200

    new: minimal intersections scene

---
+++ game/scripts/intersections_test_minimal.gd
@@ -0,0 +1,19 @@
+tool
+#extends Node
+extends "connect_intersections.gd"
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+
+	# just the intersection selection
+	connect_intersections(0,1)
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
