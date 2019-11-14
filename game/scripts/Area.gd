a03caeaa4cbe9146a4a1d1fe1be3bd7175b4f742
Zireael07 <zi@mail.com>
2019-11-14 19:02:22 +0100

    new: way to tell which intersection we're at in-game

---
+++ game/scripts/Area.gd
@@ -0,0 +1,21 @@
+extends Area
+
+# Declare member variables here. Examples:
+var player_script 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player_script = load("res://car/vehicle_player.gd")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_body_entered(body):
+	if body is VehicleBody:
+		if body is player_script:
+			print("Intersection: " + get_parent().get_name())
+	
+	#pass # Replace with function body.
