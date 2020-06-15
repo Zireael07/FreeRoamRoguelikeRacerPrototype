6cf72a9e266bba94a860e72a7eb85ec00a79cfe8
Zireael07 <zi@mail.com>
2020-06-15 16:30:17 +0200

    new: WIP vehicle dealership meshes

---
+++ game/objects/CSGCombiner.gd
@@ -0,0 +1,20 @@
+extends CSGCombiner
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_update_shape()
+	var msh = get_meshes()
+	print(msh)
+	
+	# yay GD 3
+	#msh[1].create_convex_collision()
+	
+	ResourceSaver.save("res://csg_dealer2.tres",msh[1])
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
