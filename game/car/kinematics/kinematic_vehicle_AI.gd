0e360aafd5bba4e2e86207d91d293bba1caa55ee
Zireael07 <zi@mail.com>
2022-04-09 11:49:45 +0200

    fix: missed API change

--- game/car/kinematics/kinematic_vehicle_AI.gd
+++ game/car/kinematics/kinematic_vehicle_AI.gd
@@ -366,7 +366,7 @@ func _physics_process(delta):
 		if unstick_count > 2:
 			translate_object_local(Vector3(0,0.1,0))
 			# solution from https://godotengine.org/qa/56193/how-to-manually-set-the-position-of-a-kinematicbody2d
-			set_motion_velocity(Vector3(0,gravity/10,0))
+			set_velocity(Vector3(0,gravity/10,0))
 			move_and_slide() #
 			unstick_count = 0
 			
